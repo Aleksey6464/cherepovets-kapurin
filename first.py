8166fe0056d39cc20defaa02d1b15a22b1a02fdc
Aleksey6464 <147250940+Aleksey6464@users.noreply.github.com>
2023-11-18 17:54:20 +0300

    first.py

--- first.py
+++ first.py
@@ -1,7 +1,1 @@
-git clone https://github.com/Aleksey6464/cherepovets-kapurin/new/main
-cd cherepovets-kapurin
-touch first.py 
-echo "print('My first git-repo')" >> first.py
-git add *
-git commit -am .
-git push 
+print('My first git-repo')
